7bca240d0658284d6fa47ffc105de88e5efa2494
deg997 <128143295+deg997@users.noreply.github.com>
2024-02-09 12:17:39 +0100

    Fix db Fixato il problema del duplicamento dei record. Ho diviso semplicemente lo script di popolamento in più script diversi

---
+++ db_fia/db_populator_fia/cliente_populator_fia.sql
@@ -0,0 +1,21 @@
+-- script di popolamento clienti
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Girolamo', 'Gabba', 'yBmaNPq%Qw!v5C2r', '2003-03-19', 'fernandaiannuzzi@example.org');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Luigina', 'Trupiano', '!7S6knpqUP1pWRzH', '1994-07-18', 'rdonatoni@example.org');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Michelotto', 'Mastroianni', '(dPBGx6PW42D2h4b', '1988-09-23', 'sopranocoluccio@example.net');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Veronica', 'Montanariello', 'Vj$h_OJSTcc+0+kf', '1965-02-26', 'ogarzoni@example.net');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Lolita', 'Folliero', '_B&0W4qv)p07Ouvt', '1988-05-26', 'borronigiorgia@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Ezio', 'Barcella', '&%SNz$NfQZ9%#WUQ', '2002-09-11', 'rubertomaria@example.org');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Gian', 'Ferrari', '%5iMhKyCWDqgK)vC', '2001-01-13', 'enzo18@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Lisa', 'Bignami', '^x1m^UL+FGTRUp3c', '1982-12-14', 'filogamoarmando@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Barbara', 'Interiano', '_4JFPkLo%tbLlOdw', '1984-11-06', 'marinamorpurgo@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Ricciotti', 'Frescobaldi', 'JyUocZiEY+R43+_j', '1971-11-25', 'bverdi@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Liana', 'Sobrero', '8Y7iV_)l&uLJdEwV', '1959-01-25', 'oaccardo@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Adriano', 'Merisi', ')q_*6)w9oB#ErHlx', '1969-08-08', 'flaianobernardo@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Bianca', 'Druso', 'YvO68$fB@8ijX#jq', '1963-01-22', 'wrossetti@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Imelda', 'Contrafatto', 'EEAC3s&r(fCx^wMy', '1958-09-27', 'vcociarelli@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Martina', 'Contarini', '#Qxl0SMc2A9gk5sr', '1977-06-21', 'foaroberto@example.org');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Alessandra', 'Borsellino', 'i$EcWpYdT%^(0Cvu', '1987-06-21', 'tmodugno@example.com');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Valeria', 'Cammarata', '8+*t8PnEi!5D@)td', '1989-10-03', 'ginesioaugusto@example.net');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Fortunata', 'Camiscione', 'hkm92DMx*lyB!vXa', '1989-03-02', 'doriamaria@example.org');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Giovanni', 'De Gregorio', 'hkm92DMx*lyB!vXa', '1997-10-16', 'deg@example.org');
+INSERT INTO cliente (nome, cognome, pwd, data_nascita, email) VALUES ('Davide', 'Califano', 'hkm92DMx*lyB!vXa', '1969-07-12', 'davidexoxo@example.org');
