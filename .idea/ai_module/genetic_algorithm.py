13292cfb5933b51f75003476b1aaef4952dcdd20
davidexvii-uni <d.califano18@studenti.unisa.it>
2024-02-13 04:35:42 +0100

    aggiunta output di 4 best solution

--- .idea/ai_module/genetic_algorithm.py
+++ .idea/ai_module/genetic_algorithm.py
@@ -1,7 +1,9 @@
 from data_manipulator import *
 import pygad
+import numpy
 
 # Definizione della funzione di fitness
+"""
 def fitness_func(ga_instance, solution, solution_idx):
     MA = solution[0]
     MP = solution[1]
@@ -9,7 +11,19 @@ def fitness_func(ga_instance, solution, solution_idx):
     OR = solution[3]
     fitness = (2 * OR) + (1.5 * NC) + (2 * MA) + (1.5 * MP)
     return fitness
+"""
 
+def fitness_func_batch(ga_instance, solutions, solutions_indices):
+    global number_of_calls
+    batch_fitness = []
+    for solution in solutions:
+        MA = solution[0]
+        MP = solution[1]
+        NC = solution[2]
+        OR = solution[3]
+        fitness = (2 * OR) + (1.5 * NC) + (2 * MA) + (1.5 * MP)
+        batch_fitness.append(fitness)
+    return batch_fitness
 def main():
 
     #creazione array di agricoltori
@@ -27,25 +41,28 @@ def main():
         ga_data.append(dati)
     num_individuals = 9 #numAgricoltori[0]
     num_genes = 4
-    sol_per_pop = (num_individuals, num_genes)
     # Creazione di un oggetto PyGAD per l'ottimizzazione
-    ga_instance = pygad.GA(num_generations=100,
-                           num_parents_mating=5,
-                           fitness_func=fitness_func,
+    ga_instance = pygad.GA(num_generations=20,
+                           num_parents_mating=8,
+                           fitness_func=fitness_func_batch,
+                           fitness_batch_size=4,
                            sol_per_pop=num_individuals,
                            num_genes=num_genes,
                            initial_population=ga_data,
-                           mutation_percent_genes=50)
+                           mutation_percent_genes=10,
+                           )
 
     # Avvio dell'ottimizzazione
     ga_instance.run()
-
+    best_4_solutions, best_4_indices = ga_instance.steady_state_selection(ga_instance.last_generation_fitness,
+                                                                          4)
+    best_4_fitness = ga_instance.last_generation_fitness[best_4_indices]
     # Ottieni il miglior individuo dopo l'ottimizzazione
-    solution, solution_fitness, solution_idx = ga_instance.best_solution()
-    print("Miglior soluzione:", solution)
+    solution, solution_fitness, solution_indices = ga_instance.best_solution()
+    print("Miglior soluzioni:", solution)
     print("Fitness della migliore soluzione:", solution_fitness)
-    print("solution index:", solution_idx)
-
+    print("solution index:", solution_indices)
+    print(best_4_solutions, best_4_indices, best_4_fitness)
 
 """
     ordini_agricoltore = []
