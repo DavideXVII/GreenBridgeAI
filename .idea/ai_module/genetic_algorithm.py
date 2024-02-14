bd468fa2cf445766cc8643d1897a81f4c02546fc
davidexvii-uni <132910696+davidexvii-uni@users.noreply.github.com>
2024-02-14 10:38:01 +0100

    Aggiunta commenti e modifica saturazione

--- .idea/ai_module/genetic_algorithm.py
+++ .idea/ai_module/genetic_algorithm.py
@@ -22,38 +22,33 @@ def main():
     for agricoltore_id in range (1, numAgricoltori + 1):
         nuovoagricoltore = Agricoltore(agricoltore_id)
         agricoltori.append(nuovoagricoltore)
-    for agricoltore in agricoltori:
-        print(f"id: {agricoltore.id}, fitness: {agricoltore.fitness}")
     # Creazione della popolazione iniziale
     population_size = numAgricoltori
     ids = list(range(0, numAgricoltori))  # Genera una lista di numeri da 1 a 200
-    ga_data = [ids[i:i+4] for i in range(0, len(ids), 4)]
+    ga_data = [ids[i:i+4] for i in range(0, len(ids), 4)] #Gli ID vengono inseriti 4 alla volta
     num_genes = 4
-    print(ga_data)
-    # Creazione di un oggetto PyGAD per l'ottimizzazione
+    # Creazione di un oggetto PyGAD
     ga_instance = pygad.GA(num_generations=20,
                            num_parents_mating=5,
                            fitness_func=fitness_func_wrapper(agricoltori=agricoltori),
-                           sol_per_pop=100,
                            num_genes=num_genes,
                            initial_population=ga_data,
-                           parent_selection_type = "sss",
-                           mutation_probability=0.4, #0.3 troppo poco, 0.5 troppo
+                           parent_selection_type = "sss", #Steady State Selection
+                           mutation_probability=0.4,
                            mutation_type = "random",
                            crossover_type="two_points",
                            keep_elitism = 1,
                            gene_type = int,
-                           stop_criteria=["saturate_5"],
-                           allow_duplicate_genes=False,
+                           stop_criteria=["saturate_3"], #L'algoritmo si ferma se la fitness non cambia per 5 generazioni consecutive
+                           allow_duplicate_genes=False, #Geni duplicati non ammessi
                            )
 
     # Avvio dell'ottimizzazione
     ga_instance.run()
     # Ottieni il miglior individuo dopo l'ottimizzazione
-    solution, solution_fitness, solution_indices = ga_instance.best_solution()
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
     print("Miglior soluzioni:", solution)
     print("Fitness della migliore soluzione:", solution_fitness)
-    print("solution index:", solution_indices)
 
 
 
